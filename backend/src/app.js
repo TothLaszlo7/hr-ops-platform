7c746b4f1753a54198ee3d9999342394bd95488c
TothLaszlo7 <t.laszlo1993@gmail.com>
2026-05-04 16:47:02 +0200

    feat: add employee endpoints (#3)

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,32 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const employees = [
+  { id: 1, name: "Toth Laszlo" },
+  { id: 2, name: "Fodor Klaudia" }
+];
+
+
+
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK' });
+});
+
+app.get('/employees', (req, res) => {
+  res.status(200).json(employees);
+});
+
+app.post('/employees', (req, res) => {
+  const newEmployee = {
+    id: employees.length + 1,
+    name: req.body.name
+  };
+
+  employees.push(newEmployee);
+
+  res.status(201).json(newEmployee);
 });
 
 module.exports = app;
